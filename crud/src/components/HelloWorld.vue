8c3f569d8866e54d947282f1c0a56c55daf725be
Astonark <sorel.kyrian@gmail.com>
2022-04-14 11:13:38 +0200

    ajout d'un élément

--- crud/src/components/HelloWorld.vue
+++ crud/src/components/HelloWorld.vue
@@ -1,37 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <table id="customers">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nom</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="movie in movies">
+        <td>{{ movie.id }}</td>
+        <td>{{ movie.title }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <div>
+    <input @keyup.enter="storeMovie" type="text" v-model="inputMovie.title" placeholder="Ajouter un film">
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      movies : null,
+      inputMovie: {
+        title : null
+      }
+    }
+  },
+  methods : {
+    loadMovies() {
+      axios.get('http://localhost:3000/films')
+        .then(
+            (resultat) => {
+              this.movies = resultat.data
+            }
+        )
+      .catch(
+          (erreur) => {
+            console.log(erreur)
+          }
+      )
+    },
+    storeMovie() {
+      if (this.inputMovie.title != null && this.inputMovie.title.trim() != '') {
+        axios.post('http://localhost:3000/films', this.inputMovie)
+        .then(
+            (resultat) => {
+              this.loadMovies()
+            }
+        )
+        .catch(
+            (erreur) => {
+              console.log(erreur)
+            }
+        )
+      }
+    }
+  },
+  mounted() {
+    this.loadMovies()
+  },
   props: {
     msg: String
   }
@@ -40,18 +71,36 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
 }
-li {
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
 }
 </style>
